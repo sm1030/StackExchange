1ec86af10f36d3ce7623e23e548c974deef9b037
Alexandre Malkov <sm1030@gmail.com>
2016-12-01 04:06:08 +0000

    Presenter class added

---
+++ StackExchange/StackExchange/Core/Presenter.swift
@@ -0,0 +1,60 @@
+//
+//  Presenter.swift
+//  StackExchange
+//
+//  Created by Alexandre Malkov on 01/12/2016.
+//  Copyright © 2016 Alexandre Malkov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol PresenterDelegate: class {
+    func presenterDataUpdated()
+    func presenterError(errorMessage: String)
+}
+
+class Presenter: ApiServiceDelegate {
+    
+    static let sharedInstance = Presenter()
+    
+    weak var questionsDelegate: PresenterDelegate?
+    weak var answersDelegate: PresenterDelegate?
+    var api: ApiService?
+    var questionId = 0
+    var questions = [SEQuestion]()
+    var answer: SEAnswer?
+    
+    func pullUpdates() {
+        if api == nil {
+            api = ApiService(delegate: self)
+        }
+        
+        api?.requestQuestions()
+    }
+    
+    func requestAnswer(questionId: Int) {
+        self.questionId = questionId
+        api?.requestAnswers(questionId: questionId)
+    }
+    
+    func apiServiceQuestionsReceived(questions: [SEQuestion]) {
+        self.questions = questions
+        questionsDelegate?.presenterDataUpdated()
+    }
+    
+    func apiServiceAnswersReceived(answers: SEAnswer) {
+        answersDelegate?.presenterDataUpdated()
+    }
+    
+    func apiServiceDataImageReceived() {
+        questionsDelegate?.presenterDataUpdated()
+        answersDelegate?.presenterDataUpdated()
+    }
+    
+    func apiServiceError(errorMessage: String) {
+        questionsDelegate?.presenterError(errorMessage: errorMessage)
+        answersDelegate?.presenterError(errorMessage: errorMessage)
+    }
+
+}
